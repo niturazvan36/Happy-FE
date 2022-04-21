936c5b2b8921a1e31ffaf82c0dbc6501fc19e51a
Razvan Nitu <niturazvan366@gmail.com>
2022-04-21 13:23:55 +0300

    cristi-be-homepage

--- HelloWordTest/src/app/app.component.ts
+++ HelloWordTest/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWord';
+  public items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [{
+      label: 'File',
+      items: [
+        {label: 'New', icon: 'pi pi-plus'},
+        {label: 'Open', icon: 'pi pi-download'}
+      ]
+    },
+      {
+        label: 'Edit',
+        items: [
+          {label: 'Undo', icon: 'pi pi-refresh'},
+          {label: 'Redo', icon: 'pi pi-repeat'}
+        ]
+      },
+      {
+        label: 'login',
+        items: [
+          {label: 'logIn',icon: 'pi pi-refresh'}
+        ]
+      },
+
+      ];
+  }
 }
+
+
+
+
+
--- HelloWordTest/src/app/app.module.ts
+++ HelloWordTest/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
