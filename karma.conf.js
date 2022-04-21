c826ba8113809e9273087670d84c9bdee709daca
Razvan Nitu <niturazvan366@gmail.com>
2022-04-21 14:01:46 +0300

    task_primeng

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hello-word-test'),
+      dir: require('path').join(__dirname, './coverage/happy-be'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HelloWordTest'`, () => {
+  it(`should have as title 'Happy-BE'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('HelloWordTest');
+    expect(app.title).toEqual('Happy-BE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HelloWordTest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Happy-BE app is running!');
   });
 });
